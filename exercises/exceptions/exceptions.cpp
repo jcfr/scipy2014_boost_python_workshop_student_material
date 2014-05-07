84766ff699ec04dcb3011fae4998649d44728465
Austin Bingham <austin.bingham@gmail.com>
2014-05-07 15:54:49 +0200

    Added missing string include in exceptions.cpp

--- exercises/exceptions/exceptions.cpp
+++ exercises/exceptions/exceptions.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <string>
 
 class MathError : public std::runtime_error
 {
@@ -9,7 +10,7 @@ public:
 double divide(double x, double y)
 {
     if (0 == y)
-	throw MathError("Denominator can not be 0.");
+        throw MathError("Denominator can not be 0.");
 
     return x / y;
 }
